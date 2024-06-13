c9f26b9f615200b5cfd04f78e560725be8a43e7f
nicoconti1901 <nico.conti.1901@gmail.com>
2024-06-13 14:08:08 -0300

    formulario base de login terminado

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,26 @@
-<h1>Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p class="tagline">Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form class="formulario" action="/" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Tu Email">
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Tu Password">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+        <div class="acciones">
+            <a href="/crear-cuenta">¿Aun no tienes cuenta? Crea la tuya</a>
+            <a href="/olvide-password">¿Olvidaste tu contraseña?</a>
+        </div>
+    </div><!-- .contenedor-sm -->
+
+</div>
