6c46804745831570ad5971fc6bc0ddd3c693655f
nicoconti1901 <nico.conti.1901@gmail.com>
2024-06-19 14:59:07 -0300

    vista de confirmar cuenta terminada

---
+++ views/auth/confirmar.php
@@ -0,0 +1,13 @@
+<div class="contenedor confirmar">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php' ?>
+
+    <div class="contenedor-sm">
+
+        <div class="acciones">
+            <a href="/">Inicia Sesion</a>
+
+        </div>
+
+    </div><!-- .contenedor-sm -->
+
+</div>
---
+++ views/auth/mensaje.php
@@ -0,0 +1,10 @@
+<div class="contenedor mensaje">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php' ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Hemos enviado un mail para confirmar tu cuenta UpTask</p>
+
+        
+    </div><!-- .contenedor-sm -->
+
+</div>
